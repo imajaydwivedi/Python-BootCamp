48324828f0dd9d63f97fc6224da3e8ec1a2fdc3c
Ajay Dwivedi <dwivedaj@arcesium.com>
2021-03-07 21:27:30 +0530

    Changed vscode font

--- servicecatalog/myapplication.py
+++ servicecatalog/myapplication.py
@@ -1,7 +1,4 @@
-from sqldbatools.server import help
-from sqldbatools import sqlinstance
-from sqldbatools.ldap import ldap
 
-help() # from server module
-sqlinstance.help() # from sqlinstance module
-ldap.help()
+if(1 <= 2):
+    print('less')
+
